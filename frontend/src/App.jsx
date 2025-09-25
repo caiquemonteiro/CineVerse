12d8155a47abc9827a19a166f8e2fe18033081c5
Eduardo Coelho <dudu012010@hotmail.com>
2025-09-25 19:23:25 -0300

    Ajustes do PR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import LoginPage from './pages/Login' // importa sua página de login
+// import LoginPage from './pages/Login'
+import HomePage from "./pages/Home"; 
 
 function App() {
-  return (
-    <LoginPage />
-  )
+  // return (
+  //   <LoginPage />
+  // )
+  return <HomePage />;
+
 }
 
 export default App
