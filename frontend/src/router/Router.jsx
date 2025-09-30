b7763978eaa6c36d9c876ab75818be5b292731df
Ivandro Arruda <ivandro.arrudalima@gmail.com>
2025-09-30 20:21:57 -0300

    foi adicionado a pasta utils com o arquivo constants.js contendo o token da api, foi modificado a home para receber a api dos filmes

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/";
-import LoginPage from "../pages/login/";
+import LoginPage from "../pages/Login/";
 import MoviePage from "../pages/Movie/";
 export default function AppRouter() {
   return (
