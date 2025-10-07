8d8be8580af71ccecde142e11a9a79f7109ebb3c
Ricardo <ricardo.r.rocha.outlook.com.br>
2025-10-07 00:55:24 -0300

    Atualizações no banco de dados e configuração do docker

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://cine:senha@localhost:3306/login"
+DATABASE_URL = "mysql+pymysql://cine:senha@db:3306/login"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
